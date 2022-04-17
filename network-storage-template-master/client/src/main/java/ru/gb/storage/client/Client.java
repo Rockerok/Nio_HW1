6bf82a77d87c836c173bfb989297c6eb508ed2e8
Konstantin <rockerok@mail.ru>
2022-04-17 22:50:38 +0500

    create++

--- network-storage-template-master/client/src/main/java/ru/gb/storage/client/Client.java
+++ network-storage-template-master/client/src/main/java/ru/gb/storage/client/Client.java
@@ -3,12 +3,43 @@ package ru.gb.storage.client;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-public class Client extends Application {
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+import java.time.LocalDateTime;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Client{
+    private final static ExecutorService THREAD_POOL = Executors.newFixedThreadPool(5);
+
     public static void main(String[] args) {
-        Application.launch(args);
+        try {
+            new Client().start();
+        } finally {
+            THREAD_POOL.shutdown();
+        }
     }
 
-    public void start(Stage stage) throws Exception {
-        stage.show();
+    public void start() {
+        for (int i = 0; i < 5; i++) {
+            THREAD_POOL.execute(() -> {
+                System.out.println("New client started on thread " + Thread.currentThread().getName());
+                try {
+                    SocketChannel channel = SocketChannel.open(new InetSocketAddress("localhost", 9000));
+                    while (true) {
+                        channel.write(ByteBuffer.wrap(String.format(
+                                "[%s] Message from thread %s",
+                                LocalDateTime.now(),
+                                Thread.currentThread().getName()
+                        ).getBytes()));
+                        Thread.sleep(3000);
+                    }
+                } catch (IOException | InterruptedException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
     }
 }
